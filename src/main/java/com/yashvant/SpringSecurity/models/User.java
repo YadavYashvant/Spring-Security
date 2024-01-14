f3822e441731ba5e7bdde452e27955b30360925c
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-14 23:32:22 +0530

    making models

--- src/main/java/com/yashvant/SpringSecurity/models/User.java
+++ src/main/java/com/yashvant/SpringSecurity/models/User.java
@@ -3,4 +3,16 @@ public class User {
     String username;
     String password;
     String emailId;
+
+    public User(){
+
+    }
+
+    public User(String username, String password, String emailId) {
+        this.username = username;
+        this.password = password;
+        this.emailId = emailId;
+    }
+
+
 }
