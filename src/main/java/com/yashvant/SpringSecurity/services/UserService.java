c050ac2944b8d7d8ff9374a9c86e2abc19ac88b2
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-14 23:47:27 +0530

    making userservice

---
+++ src/main/java/com/yashvant/SpringSecurity/services/UserService.java
@@ -0,0 +1,13 @@
+package com.yashvant.SpringSecurity.services;
+
+import com.yashvant.SpringSecurity.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    List<User> list = new ArrayList<>();
+
+}
