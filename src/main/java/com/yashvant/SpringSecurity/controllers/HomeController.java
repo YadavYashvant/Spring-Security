83df49846cbdb51dd0d83cb08f9df07aadc3f37e
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-21 23:59:38 +0530

    Setting up JWT authentication

---
+++ src/main/java/com/yashvant/SpringSecurity/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.yashvant.SpringSecurity.controllers;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class HomeController {
+
+    Logger logger  = LoggerFactory.getLogger(HomeController.class);
+
+}
