4f300f21af28e64b7bd20a58a092594eb71efdd1
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-26 23:06:23 +0530

    Setting up JWT authentication

--- src/main/java/com/yashvant/SpringSecurity/config/MySecurityConfig.java
+++ src/main/java/com/yashvant/SpringSecurity/config/MySecurityConfig.java
@@ -1,50 +1,34 @@
 package com.yashvant.SpringSecurity.config;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
-public class MySecurityConfig extends WebSecurityConfigurer {
-
-    private final UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.builder().
-                username("YASHVANT")
-                .password(passwordEncoder().encode("DRUGESH")).roles("ADMIN").build();
-    }
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/public").permitAll()
-                .anyRequest().authenticated()
-                .and().httpBasic();
-    }
-
+class MyConfig {
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder().
+                username("yashvant")
+                .password(passwordEncoder().encode("yashvant")).roles("ADMIN").
+                build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
+    }
+}
