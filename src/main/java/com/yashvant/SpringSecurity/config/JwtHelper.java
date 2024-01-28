6304b82c7f1860da017bfc225bdd7aa14a588436
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-28 23:48:20 +0530

    Setting up JWT authentication

--- src/main/java/com/yashvant/SpringSecurity/config/JwtHelper.java
+++ src/main/java/com/yashvant/SpringSecurity/config/JwtHelper.java
@@ -1,12 +1,15 @@
 package com.yashvant.SpringSecurity.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtHelper {
